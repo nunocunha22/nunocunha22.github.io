8cad7b8567e4ac0e3347b92cf427770767c3774f
nunocunha22 <98117765+nunocunha22@users.noreply.github.com>
2025-01-31 11:50:28 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+   output: "export",
+  basePath: "/nunocunha22.github.io",
+  images: {
+    unoptimized: true,
 };
 
 export default nextConfig;
+
